b71630391f2dbd20f5516bfac9bafc7ec2e0ea87
martin-creator <martinlubowa@outlook.com>
2024-07-12 22:28:58 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -9,9 +9,6 @@ import ProgramsComponent from "@/components/ProgramsComponent";
 import TestimonialsComponent from "@/components/TestimonialsComponent";
 
 export default function Home() {
-  const handleClick = () => {
-    window.location.href = "/contact";
-  };
   const Message = "Are you passionate about training the next generation of young scientists? Is your research organization looking to extend its training programs to young African scientists? Are you seeking to hire exceptional young scientists for your projects in Africa? Are you part of a student or professional community seeking quality training programs for your members? Partner with us to make a meaningful impact. "
   return (
     <main className="bg-backgroundLight relative">
@@ -27,7 +24,6 @@ export default function Home() {
           SectionTitle={"Partner With Us"}
           Message={Message}
           ButtonTitle={"Contact Us"}
-          ClickFunction={handleClick}
         />
         <Footer />
       </div>
